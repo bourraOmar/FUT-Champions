781dd0941893085ab69a59596b567830d611bd99
bourraOmar <bwrtmr@gmail.com>
2024-11-28 09:43:51 +0100

    add section allPlayers

--- main.js
+++ main.js
@@ -1,420 +1,3 @@
-const players = [
-  {
-    "name": "Lionel Messi",
-    "photo": "https://cdn.sofifa.net/players/158/023/25_120.png",
-    "position": "RW",
-    "nationality": "Argentina",
-    "flag": "https://cdn.sofifa.net/flags/ar.png",
-    "club": "Inter Miami",
-    "logo": "https://cdn.sofifa.net/meta/team/239235/120.png",
-    "rating": 93,
-    "pace": 85,
-    "shooting": 92,
-    "passing": 91,
-    "dribbling": 95,
-    "defending": 35,
-    "physical": 65
-  },
-  {
-    "name": "Cristiano Ronaldo",
-    "photo": "https://cdn.sofifa.net/players/020/801/25_120.png",
-    "position": "ST",
-    "nationality": "Portugal",
-    "flag": "https://cdn.sofifa.net/flags/pt.png",
-    "club": "Al Nassr",
-    "logo": "https://cdn.sofifa.net/meta/team/2506/120.png",
-    "rating": 91,
-    "pace": 84,
-    "shooting": 94,
-    "passing": 82,
-    "dribbling": 87,
-    "defending": 34,
-    "physical": 77
-  },
-  {
-    "name": "Kevin De Bruyne",
-    "photo": "https://cdn.sofifa.net/players/192/985/25_120.png",
-    "position": "CM",
-    "nationality": "Belgium",
-    "flag": "https://cdn.sofifa.net/flags/be.png",
-    "club": "Manchester City",
-    "logo": "https://cdn.sofifa.net/players/239/085/25_120.png",
-    "rating": 91,
-    "pace": 74,
-    "shooting": 86,
-    "passing": 93,
-    "dribbling": 88,
-    "defending": 64,
-    "physical": 78
-  },
-  {
-    "name": "Kylian Mbappé",
-    "photo": "https://cdn.sofifa.net/players/231/747/25_120.png",
-    "position": "ST",
-    "nationality": "France",
-    "flag": "https://cdn.sofifa.net/flags/fr.png",
-    "club": "Real Madrid",
-    "logo": "https://cdn.sofifa.net/meta/team/3468/120.png",
-    "rating": 92,
-    "pace": 97,
-    "shooting": 89,
-    "passing": 80,
-    "dribbling": 92,
-    "defending": 39,
-    "physical": 77
-  },
-  {
-    "name": "Virgil van Dijk",
-    "photo": "https://cdn.sofifa.net/players/203/376/25_120.png",
-    "position": "CB",
-    "nationality": "Netherlands",
-    "flag": "https://cdn.sofifa.net/flags/nl.png",
-    "club": "Liverpool",
-    "logo": "https://cdn.sofifa.net/meta/team/8/120.png",
-    "rating": 90,
-    "pace": 75,
-    "shooting": 60,
-    "passing": 70,
-    "dribbling": 72,
-    "defending": 92,
-    "physical": 86
-  },
-  {
-    "name": "Antonio Rudiger",
-    "photo": "https://cdn.sofifa.net/players/205/452/25_120.png",
-    "position": "CB",
-    "nationality": "Germany",
-    "flag": "https://cdn.sofifa.net/flags/de.png",
-    "club": "Real Madrid",
-    "logo": "https://cdn.sofifa.net/meta/team/3468/120.png",
-    "rating": 88,
-    "pace": 82,
-    "shooting": 55,
-    "passing": 73,
-    "dribbling": 70,
-    "defending": 86,
-    "physical": 86
-  },
-  {
-    "name": "Neymar Jr",
-    "photo": "https://cdn.sofifa.net/players/190/871/25_120.png",
-    "position": "LW",
-    "nationality": "Brazil",
-    "flag": "https://cdn.sofifa.net/flags/br.png",
-    "club": "Al-Hilal",
-    "logo": "https://cdn.sofifa.net/meta/team/7011/120.png",
-    "rating": 90,
-    "pace": 91,
-    "shooting": 83,
-    "passing": 86,
-    "dribbling": 94,
-    "defending": 37,
-    "physical": 61
-  },
-  {
-    "name": "Mohamed Salah",
-    "photo": "https://cdn.sofifa.net/players/192/985/25_120.png",
-    "position": "RW",
-    "nationality": "Egypt",
-    "flag": "https://cdn.sofifa.net/flags/eg.png",
-    "club": "Liverpool",
-    "logo": "https://cdn.sofifa.net/meta/team/8/120.png",
-    "rating": 89,
-    "pace": 93,
-    "shooting": 87,
-    "passing": 81,
-    "dribbling": 90,
-    "defending": 45,
-    "physical": 75
-  },
-  {
-    "name": "Joshua Kimmich",
-    "photo": "https://cdn.sofifa.net/players/212/622/25_120.png",
-    "position": "CM",
-    "nationality": "Germany",
-    "flag": "https://cdn.sofifa.net/flags/de.png",
-    "club": "Bayern Munich",
-    "logo": "https://cdn.sofifa.net/meta/team/503/120.png",
-    "rating": 89,
-    "pace": 70,
-    "shooting": 75,
-    "passing": 88,
-    "dribbling": 84,
-    "defending": 84,
-    "physical": 81
-  },
-  {
-    "name": "Jan Oblak",
-    "photo": "https://cdn.sofifa.net/players/200/389/25_120.png",
-    "position": "GK",
-    "nationality": "Slovenia",
-    "flag": "https://cdn.sofifa.net/flags/si.png",
-    "club": "Atletico Madrid",
-    "logo": "https://cdn.sofifa.net/meta/team/7980/120.png",
-    "rating": 91,
-    "diving": 89,
-    "handling": 90,
-    "kicking": 78,
-    "reflexes": 92,
-    "speed": 50,
-    "positioning": 88
-  },
-  {
-    "name": "Luka Modrić",
-    "photo": "https://cdn.sofifa.net/players/177/003/25_120.png",
-    "position": "CM",
-    "nationality": "Croatia",
-    "flag": "https://cdn.sofifa.net/flags/hr.png",
-    "club": "Real Madrid",
-    "logo": "https://cdn.sofifa.net/meta/team/3468/120.png",
-    "rating": 88,
-    "pace": 74,
-    "shooting": 78,
-    "passing": 89,
-    "dribbling": 90,
-    "defending": 72,
-    "physical": 65
-  },
-  {
-    "name": "Vinicius Junior",
-    "photo": "https://cdn.sofifa.net/players/238/794/25_120.png",
-    "position": "LW",
-    "nationality": "Brazil",
-    "flag": "https://cdn.sofifa.net/flags/br.png",
-    "club": "Real Madrid",
-    "logo": "https://cdn.sofifa.net/meta/team/3468/120.png",
-    "rating": 89,
-    "pace": 91,
-    "shooting": 88,
-    "passing": 85,
-    "dribbling": 94,
-    "defending": 39,
-    "physical": 61
-  },
-  {
-    "name": "Brahim Diáz",
-    "photo": "https://cdn.sofifa.net/players/231/410/25_120.png",
-    "position": "LW",
-    "nationality": "Morocco",
-    "flag": "https://cdn.sofifa.net/flags/ma.png",
-    "club": "Real Madrid",
-    "logo": "https://cdn.sofifa.net/meta/team/3468/120.png",
-    "rating": 82,
-    "pace": 85,
-    "shooting": 74,
-    "passing": 78,
-    "dribbling": 85,
-    "defending": 31,
-    "physical": 56
-  },
-  {
-    "name": "Karim Benzema",
-    "photo": "https://cdn.sofifa.net/players/165/153/25_120.png",
-    "position": "ST",
-    "nationality": "France",
-    "flag": "https://cdn.sofifa.net/flags/fr.png",
-    "club": "Al-Ittihad",
-    "logo" :"https://cdn.sofifa.net/meta/team/476/120.png",
-    "rating": 90,
-    "pace": 77,
-    "shooting": 90,
-    "passing": 83,
-    "dribbling": 88,
-    "defending": 40,
-    "physical": 78
-  },
-  {
-    "name": "Erling Haaland",
-    "photo": "https://cdn.sofifa.net/players/239/085/25_120.png",
-    "position": "ST",
-    "nationality": "Norway",
-    "flag": "https://cdn.sofifa.net/flags/no.png",
-    "club": "Manchester City",
-    "logo": "https://cdn.sofifa.net/players/239/085/25_120.png",
-    "rating": 91,
-    "pace": 89,
-    "shooting": 94,
-    "passing": 65,
-    "dribbling": 80,
-    "defending": 45,
-    "physical": 88
-  },
-  {
-    "name": "N'Golo Kanté",
-    "photo": "https://cdn.sofifa.net/players/215/914/25_120.png",
-    "position": "CDM",
-    "nationality": "France",
-    "flag": "https://cdn.sofifa.net/flags/fr.png",
-    "club": "Al-Ittihad",
-    "logo": "https://cdn.sofifa.net/meta/team/476/120.png",
-    "rating": 87,
-    "pace": 77,
-    "shooting": 66,
-    "passing": 75,
-    "dribbling": 82,
-    "defending": 88,
-    "physical": 82
-  },
-  {
-    "name": "Alphonso Davies",
-    "photo": "https://cdn.sofifa.net/players/234/396/25_120.png",
-    "position": "LB",
-    "nationality": "Canada",
-    "flag": "https://cdn.sofifa.net/flags/ca.png",
-    "club": "Bayern Munich",
-    "logo": "https://cdn.sofifa.net/meta/team/503/120.png",
-    "rating": 84,
-    "pace": 96,
-    "shooting": 68,
-    "passing": 77,
-    "dribbling": 82,
-    "defending": 76,
-    "physical": 77
-  },
-  {
-    "name": "Yassine Bounou",
-    "photo": "https://cdn.sofifa.net/players/209/981/25_120.png",
-    "position": "GK",
-    "nationality": "Morocco",
-    "flag": "https://cdn.sofifa.net/flags/ma.png",
-    "club": "Al-Hilal",
-    "logo": "https://cdn.sofifa.net/meta/team/7011/120.png",
-    "rating": 84,
-    "diving": 81,
-    "handling": 82,
-    "kicking": 77,
-    "reflexes": 86,
-    "speed": 38,
-    "positioning": 83
-  },
-  {
-    "name": "Bruno Fernandes",
-    "photo": "https://cdn.sofifa.net/players/212/198/25_120.png",
-    "position": "CM",
-    "nationality": "Portugal",
-    "flag": "https://cdn.sofifa.net/flags/pt.png",
-    "club": "Manchester United",
-    "logo": "https://cdn.sofifa.net/meta/team/14/120.png",
-    "rating": 88,
-    "pace": 75,
-    "shooting": 85,
-    "passing": 89,
-    "dribbling": 84,
-    "defending": 69,
-    "physical": 77
-  },
-  {
-    "name": "Jadon Sancho",
-    "photo": "https://cdn.sofifa.net/players/233/049/25_120.png",
-    "position": "LW",
-    "nationality": "England",
-    "flag": "https://cdn.sofifa.net/flags/gb-eng.png",
-    "club": "Manchester United",
-    "logo": "https://cdn.sofifa.net/meta/team/14/120.png",
-    "rating": 84,
-    "pace": 85,
-    "shooting": 74,
-    "passing": 78,
-    "dribbling": 88,
-    "defending": 34,
-    "physical": 63
-  },
-  {
-    "name": "Trent Alexander-Arnold",
-    "photo": "https://cdn.sofifa.net/players/231/281/25_120.png",
-    "position": "RB",
-    "nationality": "England",
-    "flag": "https://cdn.sofifa.net/flags/gb-eng.png",
-    "club": "Liverpool",
-    "rating": 87,
-    "pace": 76,
-    "shooting": 66,
-    "passing": 89,
-    "dribbling": 80,
-    "defending": 79,
-    "physical": 71
-  },
-  {
-    "name": "Achraf Hakimi",
-    "photo": "https://cdn.sofifa.net/players/235/212/25_120.png",
-    "position": "RB",
-    "nationality": "Morocco",
-    "flag": "https://cdn.sofifa.net/flags/ma.png",
-    "club": "Paris Saint-Germain",
-    "logo": "https://cdn.sofifa.net/meta/team/591/120.png",
-    "rating": 84,
-    "pace": 91,
-    "shooting": 76,
-    "passing": 80,
-    "dribbling": 80,
-    "defending": 75,
-    "physical": 78
-  },
-  {
-    "name": "Youssef En-Nesyri",
-    "photo": "https://cdn.sofifa.net/players/235/410/25_120.png",
-    "position": "ST",
-    "nationality": "Morocco",
-    "flag": "https://cdn.sofifa.net/flags/ma.png",
-    "club": "Fenerbahçe",
-    "logo": "https://cdn.sofifa.net/meta/team/88/120.png",
-    "rating": 83,
-    "pace": 82,
-    "shooting": 82,
-    "passing": 63,
-    "dribbling": 77,
-    "defending": 36,
-    "physical": 80
-  },
-  {
-    "name": "Noussair Mazraoui",
-    "photo": "https://cdn.sofifa.net/players/236/401/25_120.png",
-    "position": "LB",
-    "nationality": "Morocco",
-    "flag": "https://cdn.sofifa.net/flags/ma.png",
-    "club": "Manchester United",
-    "logo": "https://cdn.sofifa.net/meta/team/14/120.png",
-    "rating": 81,
-    "pace": 78,
-    "shooting": 66,
-    "passing": 77,
-    "dribbling": 83,
-    "defending": 77,
-    "physical": 71
-  },
-  {
-    "name": "Ismael Saibari",
-    "photo": "https://cdn.sofifa.net/players/259/480/25_120.png",
-    "position": "CM",
-    "nationality": "Morocco",
-    "flag": "https://cdn.sofifa.net/flags/ma.png",
-    "club": "PSV",
-    "logo": "https://cdn.sofifa.net/meta/team/682/120.png",
-    "rating": 83,
-    "pace": 89,
-    "shooting": 78,
-    "passing": 80,
-    "dribbling": 86,
-    "defending": 55,
-    "physical": 84
-  },
-  {
-    "name": "Gianluigi Donnarumma",
-    "photo": "https://cdn.sofifa.net/players/230/621/25_120.png",
-    "position": "GK",
-    "nationality": "Italy",
-    "flag": "https://cdn.sofifa.net/flags/it.png",
-    "club": "Paris Saint-Germain",
-    "logo": "https://cdn.sofifa.net/meta/team/591/120.png",
-    "rating": 89,
-    "diving": 88,
-    "handling": 84,
-    "kicking": 75,
-    "reflexes": 90,
-    "speed": 50,
-    "positioning": 85
-  }
-]
 console.log(players);
 
 let teamInput = document.getElementById('teamPlan');
@@ -455,10 +38,18 @@ let inputNastion = document.getElementById('Nationality');
 let inputClub = document.getElementById('Club');
 let inputPost = document.getElementById('Position');
 
+let paceInput = document.getElementById('Pace');
+let hooInput = document.getElementById('Hoo');
+let pasInput = document.getElementById('Pas');
+let driInput = document.getElementById('Dri');
+let defInput = document.getElementById('Def');
+let phyInput = document.getElementById('Phy');
+
 let array = [];
 let index = -1;
- 
 
+const createCard = document.querySelector('#card');
+ 
 
 
 
@@ -472,15 +63,81 @@ closePopup.addEventListener("click",()=>{
 });
 
 
-createForm.addEventListener("click",(e) => {
-  e.preventDefault();
-  if(inputName.value !== "" || inputNation.value !== "" || inputClub.value !== "" || inputPost.value !== ""){
-      array[index] = {
-        Name : inputName.value,
-        Nation : inputNation.value,
-        Club : inputClub.value,
-        Post : inputPost.value,
-      };
-      index = -1;
+creatForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (inputName.value !== "" || inputNation.value !== "" || inputClub.value !== "" || inputPost.value !== "") {
+      add_to_array(inputName.value, inputNation.value, inputClub.value, inputPost.value);
+      renderList();
+
+    }
+    else {
+      alert("Complete the form!");
+    }
+  });
+
+
+  function add_to_list(Name,Nation,Club,Post,pace,hoo,pas,dri,def,phy){
+    let card_element = {
+      Name : Name,
+      Nation : Nation,
+      Club : Club,
+      Post : Post,
+      pace : pace,
+      hoo : hoo,
+      pas : pas,
+      dri : dri,
+      def : def,
+      phy : phy,
+    };
+    array.push(card_element);
+
+    inputName.value = "";
+    inputNation.value = "";
+    inputClub.value = "";
+    inputPost.value = "";
+    paceInput.value = "";
+    hooInput.value = "";
+    pasInput.value = "";
+    driInput.value = "";
+    defInput.value = "";
+    phyInput.value = "";
   }
-})
+  function renderList(){
+    createCart.innerHTML = "";
+
+    array.forEach((card_element, index) => {
+      let div = decument.creatElement("div");
+      div.innerHTML = `
+        <div class="relative flex flex-col justify-center mt-3 md:mt-0 md:w-24 md:h-32 w-10 h-12 bg-[url('img/card-normal.webp')] bg-no-repeat bg-cover bg-center">
+              <div class="scale-[2] ml-9 mt-[0.3rem]">
+                <div class="absolute ml-1 mt-2">
+                  <p class="text-[0.3rem] font-bold">90</p>
+                  <p class="text-[0.2rem] ">${inputPost.value}</p>
+                </div>
+                <img class="w-7 ml-2 mt-1 z-20 " src="img/player.webp" alt="">
+                <p class="absolute font-semibold text-[0.2rem] ml-[1rem]">${inputName.value}</p>
+                <div class="flex ml-[0.4rem] mt-[0.3rem] gap-[0.07rem]">
+                  <p class="text-[0.13rem]">PAC</p>
+                  <p class="text-[0.13rem]">SHO</p>
+                  <p class="text-[0.13rem]">PAS</p>
+                  <p class="text-[0.13rem]">DRI</p>
+                  <p class="text-[0.13rem]">DEF</p>
+                  <p class="text-[0.13rem]">PHY</p>
+                </div>
+                <div class="flex ml-[0.4rem] mt-[-0.05rem] gap-[0.1rem]">
+                  <p class="text-[0.17rem] font-bold">${paceInput.value}</p>
+                  <p class="text-[0.17rem] font-bold">${hooInput.value}</p>
+                  <p class="text-[0.17rem] font-bold">${pasInput.value}</p>
+                  <p class="text-[0.17rem] font-bold">${driInput.value}</p>
+                  <p class="text-[0.17rem] font-bold">${defInput.value}</p>
+                  <p class="text-[0.17rem] font-bold">${phyInput.value}</p>
+                </div>
+                <div class="flex ml-[0.9rem] mt-[0.1rem] gap-[0.4rem]">
+                  <img class="w-[0.21rem] " src="img/120.webp" alt="">
+                  <img class="h-[0.2rem] w-[0.3rem] mt-[]" src="img/ar.webp" alt="">
+                </div>
+              </div>
+            </div>
+      `
+    })
+  }
